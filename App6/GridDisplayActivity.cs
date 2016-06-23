f02e88e11de5d5a009aae890e6835a0023cd6ae7
Zed Coder <kabilam@geospatialdms.com>
2016-06-23 03:52:17 +0200

    attempt at diplaying a custom list view

--- App6/GridDisplayActivity.cs
+++ App6/GridDisplayActivity.cs
@@ -21,23 +21,73 @@ namespace JhpDataSystem
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Create your application here
-            //SetContentView(Resource.Layout.griddisplay);
-            //var gridview = FindViewById<GridView>(Resource.Id.gridview);
             var all = new LocalDB3().DB
                 .Table<PrepexClientSummary>()
                 .OrderBy(t => t.PlacementDate)
                 .ToList();
             clients = all;
-
-            //List<object> sArray = new List<object>{ "This", "is", 3.5, true, 2, "for", "bla" };
-            //var adp = new ArrayAdapter(this, Resource.Layout.griddisplay, all);
-            ListAdapter = new ArrayAdapter<PrepexClientSummary>(this, 
-                Android.Resource.Layout.SimpleListItem1, all);
-            //gridview.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
-            //    Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
-            //};
+            var adapter = new ClientSummaryAdapter(this);
+            adapter._myList.AddRange(clients);
+            //ListAdapter = new ArrayAdapter<PrepexClientSummary>(this, 
+            //    Android.Resource.Layout.SimpleListItem1, all);
+            ListView.FastScrollEnabled = true;
         }
+
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            var t = clients[position];
+            Android.Widget.Toast.MakeText(this, t.Names, Android.Widget.ToastLength.Short).Show();
+        }
+
+        public class ClientSummaryAdapter : BaseAdapter<PrepexClientSummary>
+        {
+            ListActivity _context;
+            public ClientSummaryAdapter(ListActivity context)
+            {
+                _context = context;
+            }
+
+            public 
+
+            List<PrepexClientSummary> _myList = new List<PrepexClientSummary>();
+            public override PrepexClientSummary this[int position]
+            {
+                get
+                {
+                    return _myList[position];
+                }
+            }
+
+            public override int Count
+            {
+                get
+                {
+                    return _myList.Count;
+                }
+            }
+
+            public override long GetItemId(int position)
+            {
+                return _myList[position].Id.Value.GetHashCode();
+            }
+
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                if (convertView == null)
+                {
+                    convertView = _context.LayoutInflater.Inflate(Resource.Layout.clientsummary, parent);
+                }
+                return convertView;
+            }
+        }
+
+        //public override View GetView(int position, View convertView, ViewGroup parent)
+        //{
+        //    View view = convertView; // re-use an existing view, if one is available
+        //    if (view == null) // otherwise create a new one
+        //        view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+        //    view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = items[position];
+        //    return view;
+        //}
     }
 }
